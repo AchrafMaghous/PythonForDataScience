11e489c370faf98013edb7079fc229f19aa4d17b
AchrafMaghous <acmaghou@e3r4p11.1337.ma>
2024-02-26 10:54:04 +0100

    added comments for ex00 and starting now the rest

---
+++ Python-1-Array/ex00/give_bmi.py
@@ -0,0 +1,38 @@
+import sys
+import numpy as np
+
+
+def check_is_int_or_float(arr: list[int | float]) -> bool:
+    """
+    @param arr: list[int | float]
+    @return: bool
+    @description: Check if the elements of the list are either int or float.
+    """
+    for i in arr:
+        if not isinstance(i, (int, float)):
+            return False
+    return True
+
+
+def give_bmi(height: list[int | float], weight: list[int | float]) -> list[int | float]:
+    """
+    @param height: list[int | float]
+    @param weight: list[int | float]
+    @return: list[int | float]
+    @description: Calculate BMI from the given height and weight.
+    """
+    if len(height) != len(weight):
+        print("Error: The length of the two lists should be the same.")
+        sys.exit(1)
+    if not check_is_int_or_float(height) or not check_is_int_or_float(weight):
+        print("Error: The elements of the lists should be either int or float.")
+        sys.exit(1)
+    height_arr = np.array(height)
+    weight_arr = np.array(weight)
+    bmi = weight_arr / (height_arr ** 2)
+    return bmi.tolist()
+
+def apply_limit(bmi: list[int | float], limit: int) -> list[bool]:
+    bmi_arr = np.array(bmi)
+    return (bmi_arr > limit).tolist()
+
